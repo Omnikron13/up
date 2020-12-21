a2168725fc6a087ebd34dbc5b8922c116415211a
Joey Sabey <joey.sabey@gmx.com>
2020-12-21 20:34:59 +0000

    Added raw_windows.go to provided uptime data for windows machines. God knows how portable it is between versions tho...

---
+++ raw_windows.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"golang.org/x/sys/windows"
+)
+
+
+const (
+	DLL_NAME  = "kernel32.dll"
+	PROC_NAME = "GetTickCount64"
+	NANO = 1000
+)
+
+
+var (
+	_Kernel32 = windows.MustLoadDLL(DLL_NAME)
+	_GetTickCount64 = _Kernel32.MustFindProc(PROC_NAME)
+)
+
+
+// Get the raw uptime count.
+// Windows favours milliseconds, so it needs multiplying by 1,000 to
+// standardise against nanoseconds (the smallest units any system
+// will be likely use for anything).
+func GetRawUptime() (uint64, error) {
+	tick, err := GetTickCount64()
+	return tick * NANO, err
+}
+
+
+// Extra raw system call wrapper
+func GetTickCount64() (ms uint64, e error) {
+	raw, _, status:= _GetTickCount64.Call()
+	ms = uint64(raw)
+	if ms == 0 {
+		e = fmt.Errorf("Zeroday error! 0 mseconds returned, code: %d", status)
+	}
+	return ms, e
+}
+
+
