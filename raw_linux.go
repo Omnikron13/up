d2421642fbb827e3e31c0499374e38b550b78613
Joey Sabey <joey.sabey@gmx.com>
2020-12-21 20:31:17 +0000

    Moved GetRawUptime(), BytesToInt(), & BytesToFloat() out into raw_linux.go - starting portability work

---
+++ raw_linux.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+    "bytes"
+    "errors"
+    "io/ioutil"
+)
+
+
+// Get the raw uptime seconds count.
+// If OS other than linux are to be supported, it would be here.
+func GetRawUptime() (float64, error) {
+    s, err := ioutil.ReadFile("/proc/uptime")
+    if err != nil {
+        return 0, err
+    }
+    f, err := BytesToFloat(s[:bytes.IndexByte(s, ' ')])
+    return f, err
+}
+
+
+// Convert a byte slice of ASCII numerals into a float.
+func BytesToFloat(s []byte) (float64, error) {
+    // Find the decimal point
+    n := bytes.IndexByte(s, '.')
+    // ...or just process as an int if there isn't one
+    if n == -1 {
+        i, err := BytesToInt(s)
+        return float64(i), err
+    }
+    // ...and count decimal places
+    dp := (len(s) - n) - 1
+
+    // Read the integer section
+    i, err := BytesToInt(s[:n])
+    if err != nil {
+        return 0, err
+    }
+    f := float64(i)
+    // ...and the decimals
+    i, err = BytesToInt(s[n+1:])
+    if err != nil {
+        return 0, err
+    }
+    f += float64(i) / math.Pow10(dp)
+
+    return f, nil
+}
+
+
+// Convert a byte slice of ASCII numerals into an integer.
+func BytesToInt(s []byte) (int, error) {
+    var i int
+    for _, v := range s {
+        if v < '0' || v > '9' {
+            return 0, errors.New("NaN")
+        }
+        i *= 10
+        i += int(v - '0')
+    }
+    return i, nil
+}
+
+
--- up.go
+++ up.go
@@ -1,10 +1,7 @@
 package main
 
 import (
-    "bytes"
-    "errors"
     "fmt"
-    "io/ioutil"
     "math"
 )
 
@@ -36,66 +33,11 @@ func main() {
 }
 
 
-// Convert a byte slice of ASCII numerals into a float.
-func BytesToFloat(s []byte) (float64, error) {
-    // Find the decimal point
-    n := bytes.IndexByte(s, '.')
-    // ...or just process as an int if there isn't one
-    if n == -1 {
-        i, err := BytesToInt(s)
-        return float64(i), err
-    }
-    // ...and count decimal places
-    dp := (len(s) - n) - 1
-
-    // Read the integer section
-    i, err := BytesToInt(s[:n])
-    if err != nil {
-        return 0, err
-    }
-    f := float64(i)
-    // ...and the decimals
-    i, err = BytesToInt(s[n+1:])
-    if err != nil {
-        return 0, err
-    }
-    f += float64(i) / math.Pow10(dp)
-
-    return f, nil
-}
-
-
-// Convert a byte slice of ASCII numerals into an integer.
-func BytesToInt(s []byte) (int, error) {
-    var i int
-    for _, v := range s {
-        if v < '0' || v > '9' {
-            return 0, errors.New("NaN")
-        }
-        i *= 10
-        i += int(v - '0')
-    }
-    return i, nil
-}
-
-
 // TODO: rework this (and related) to work with ints for cleaner calculations?
 // Get number of units of u seconds from t.
 func ExtractUnit(t float64, u float64) (uint, float64) {
     i := math.Floor(t / u)
     return uint(i), t - i * u
-}
-
-
-// Get the raw uptime seconds count.
-// If OS other than linux are to be supported, it would be here.
-func GetRawUptime() (float64, error) {
-    s, err := ioutil.ReadFile("/proc/uptime")
-    if err != nil {
-        return 0, err
-    }
-    f, err := BytesToFloat(s[:bytes.IndexByte(s, ' ')])
-    return f, err
 }
 
 
